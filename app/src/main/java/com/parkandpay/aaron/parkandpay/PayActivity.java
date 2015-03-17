1fcaff665231d791770eb1f72db15a6b3650c3e1
Jessica Opaleski <jopalesk@umich.edu>
2015-03-17 01:27:05 -0400

    Time display is now clickable - it opens the time dialogue. Make payment handles only the backend and toast

--- app/src/main/java/com/parkandpay/aaron/parkandpay/PayActivity.java
+++ app/src/main/java/com/parkandpay/aaron/parkandpay/PayActivity.java
@@ -171,73 +171,74 @@ public class PayActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void onClickPay(View view) {
-        Log.d("test", "pay button clicked");
-
+    public void onClickAddTime(View view) {
+        Log.d("test", "add time button clicked");
 
         final Calendar c = Calendar.getInstance();
         int curHour = c.get(Calendar.HOUR_OF_DAY);
         Integer curMinute = c.get(Calendar.MINUTE);
         TimePickerDialog tpd = new TimePickerDialog(this,
-                new TimePickerDialog.OnTimeSetListener() {
+            new TimePickerDialog.OnTimeSetListener() {
 
-                    @Override
-                    public void onTimeSet(TimePicker view, int hourOfDay,
-                                          int minute) {
-                        // this is an async callback
-                        Log.d("test", "should be second: " + Integer.toString(hourOfDay));
-                        selectedTime = new Date();
-                        Calendar tempTime = Calendar.getInstance();
-                        tempTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
-                        tempTime.set(Calendar.MINUTE, minute);
-                        selectedTime = tempTime.getTime();
+                @Override
+                public void onTimeSet(TimePicker view, int hourOfDay,
+                                      int minute) {
+                    // this is an async callback
+                    Log.d("test", "should be second: " + Integer.toString(hourOfDay));
+                    Log.d("test", selectedTime.toString());
 
+                    selectedTime = new Date();
+                    Calendar tempTime = Calendar.getInstance();
+                    tempTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                    tempTime.set(Calendar.MINUTE, minute);
+                    selectedTime = tempTime.getTime();
 
-                        Log.d("test", selectedTime.toString());
-                        SimpleDateFormat format = new SimpleDateFormat("MMMM dd, hh:mm a");
-                        // Probably want a confirmation here
-                        ParseObject dataObject = ParseObject.createWithoutData("ParkingSpot", selectedLotObj.getObjectId());
-
-                        dataObject.put("PaidForUntil", selectedTime);
-                        dataObject.saveInBackground(new SaveCallback() {
-                            public void done(ParseException e) {
-                                if (e == null) {
-                                    // saved successfully
-                                    Log.d("test", "parse object saved");
-                                    Context context = getApplicationContext();
-
-                                    //make pretty time
-                                    SimpleDateFormat dateTimeFormat = new SimpleDateFormat("MMMM dd, hh:mm a");
-                                    String dateTime = "Expires at: " + dateTimeFormat.format(selectedTime);
-
-                                    int duration = Toast.LENGTH_LONG;
-                                    Toast toast = Toast.makeText(context, "Payment Successful!\n"+dateTime, duration);
-                                    toast.show();
-
-                                    SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm a");
-                                    String time = timeFormat.format(selectedTime);
-                                    final TextView time_remaining_text = (TextView) findViewById(R.id.time_remaining_text);
-                                    time_remaining_text.setText(time);
-
-                                    resetSpotButton.setVisibility(View.VISIBLE);
-
-                                    ApplicationConfig.setSpotTaken(spotNum);
-                                    ApplicationConfig.setLotName(lotName);
-
-                                } else {
-                                    System.out.println(e.getMessage());
-                                    System.out.println("Object: " + selectedLotObj.getObjectId());
-                                    //did not save
-                                    Toast toast = Toast.makeText(getApplicationContext(), "Error: Unable to Buy Spot!", Toast.LENGTH_LONG);
-                                    toast.show();
-                                    Log.d("error", e.toString());
-                                }
-                            }
-                        });
-
-
-                    }
-                }, curHour, curMinute, false);
+                    SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm a");
+                    String time = timeFormat.format(selectedTime);
+                    final TextView time_remaining_text = (TextView) findViewById(R.id.time_remaining_text);
+                    time_remaining_text.setText(time);
+                }
+            }, curHour, curMinute, false);
         tpd.show();
+    }
+
+    public void onClickPay(View view) {
+        Log.d("test", "pay button clicked");
+
+        // Probably want a confirmation here
+        ParseObject dataObject = ParseObject.createWithoutData("ParkingSpot", selectedLotObj.getObjectId());
+
+        dataObject.put("PaidForUntil", selectedTime);
+        dataObject.saveInBackground(new SaveCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    // saved successfully
+                    Log.d("test", "parse object saved");
+                    Context context = getApplicationContext();
+
+                    //make pretty time
+                    SimpleDateFormat dateTimeFormat = new SimpleDateFormat("MMMM dd, hh:mm a");
+                    String dateTime = "Expires at: " + dateTimeFormat.format(selectedTime);
+
+                    int duration = Toast.LENGTH_LONG;
+                    Toast toast = Toast.makeText(context, "Payment Successful!\n"+dateTime, duration);
+                    toast.show();
+
+                    resetSpotButton.setVisibility(View.VISIBLE);
+
+                    ApplicationConfig.setSpotTaken(spotNum);
+                    ApplicationConfig.setLotName(lotName);
+
+                } else {
+                    System.out.println(e.getMessage());
+                    System.out.println("Object: " + selectedLotObj.getObjectId());
+                    //did not save
+                    Toast toast = Toast.makeText(getApplicationContext(), "Error: Unable to Buy Spot!", Toast.LENGTH_LONG);
+                    toast.show();
+                    Log.d("error", e.toString());
+                }
+            }
+        });
+
     }
 }
